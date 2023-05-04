0f49fc3db44868c82b33c4a00d7644fc2cac42cb
JRcd95 <joanieruchaud@gmail.com>
2023-05-04 16:44:07 +0200

    Création des entités pour la position et le déplacement du personnage

---
+++ src/fr/exerciceAventurier/entities/Way.java
@@ -0,0 +1,18 @@
+package fr.exerciceAventurier.entities;
+
+public class Way {
+	
+	private String way;
+	
+	public Way(String way) {
+		this.setWay(way);
+	}
+
+	public String getWay() {
+		return way;
+	}
+
+	public void setWay(String way) {
+		this.way = way;
+	}
+}
